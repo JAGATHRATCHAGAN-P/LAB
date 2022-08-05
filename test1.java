3a18a021d28a2a8a5252c14e58a05bbe78e540dd
JAGATHRATCHAGAN-P <110395817+JAGATHRATCHAGAN-P@users.noreply.github.com>
2022-08-05 01:16:26 -0700

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,11 @@
+class test1
+{
+public static void main(String[]args)
+{
+   System.out.println("Hello world");
+   System.out.println(15);
+   System.out.println(17.5F);
+   System.out.println(12345790987654321L);
+   System.out.println(true);
+}
+}   
